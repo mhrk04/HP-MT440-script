47e71edaa1c45bc38852d05bf32d1f4264b6c6a3
mhrk04 <haziqrohaizan@gmail.com>
2025-04-08 15:09:21 +0800

    add snyc time function

--- group-policy.bat
+++ group-policy.bat
@@ -1,4 +1,15 @@
 @echo off
+
+REM === Sync time with NTP server ===
+echo Syncing time with time.windows.com...
+net stop w32time >nul
+w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:manual /reliable:yes /update
+net start w32time >nul
+w32tm /resync
+echo Time sync complete.
+
+
+
 REM === Restrict all removable media for Computer ===
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices" /v Deny_All /t REG_DWORD /d 1 /f
 
